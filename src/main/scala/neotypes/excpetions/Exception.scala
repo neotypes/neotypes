e65d019b43febdcc41f0c7ee6db40be94c5ca643
Dmitry Fedosov <dimafeng@gmail.com>
2018-12-16 21:28:49 -0500

    Exception hierarchy

--- src/main/scala/neotypes/excpetions/Exception.scala
+++ src/main/scala/neotypes/excpetions/Exception.scala
@@ -2,10 +2,12 @@ package neotypes.excpetions
 
 import org.neo4j.driver.v1.exceptions.value.Uncoercible
 
-case class PropertyNotFoundException(message: String) extends Exception(message)
+sealed class NeotypesException(message: String, cause: Throwable = this) extends Exception(message, cause)
 
-case class ConversionException(message: String) extends Exception(message)
+case class PropertyNotFoundException(message: String) extends NeotypesException(message)
 
-case class NoFieldsDefinedException(message: String) extends Exception(message)
+case class ConversionException(message: String) extends NeotypesException(message)
 
-case class UncoercibleException(message: String, cause: Uncoercible) extends Exception(message, cause)
+case class NoFieldsDefinedException(message: String) extends NeotypesException(message)
+
+case class UncoercibleException(message: String, cause: Uncoercible) extends NeotypesException(message, cause)
