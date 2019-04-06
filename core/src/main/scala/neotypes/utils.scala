1ad0e7b7c94079a1c0861bb361ee6c8e00f4ef5d
Luis Miguel Mejía Suárez <luis.miguel.mejia@hotmail.com>
2019-04-06 17:30:20 -0500

    Refactor core.utils

--- core/src/main/scala/neotypes/utils.scala
+++ core/src/main/scala/neotypes/utils.scala
@@ -1,23 +1,32 @@
 package neotypes.utils
 
 import java.util.concurrent.CompletionException
+import java.util.function.{Consumer => JConsumer, Function => JFunction}
 
 object FunctionUtils {
-  implicit def function[T, U](f: Function[T, U]): java.util.function.Function[T, U] = new java.util.function.Function[T, U] {
-    override def apply(t: T): U = f(t)
-  }
+  implicit def function[T, U](f: Function[T, U]): JFunction[T, U] =
+    new JFunction[T, U] {
+      override def apply(t: T): U = f(t)
+    }
 
-  implicit def consumer[T](f: Function[T, Unit]): java.util.function.Consumer[T] = new java.util.function.Consumer[T] {
-    override def accept(t: T): Unit = f(t)
-  }
-
+  implicit def consumer[T](f: Function[T, Unit]): JConsumer[T] =
+    new JConsumer[T] {
+      override def accept(t: T): Unit = f(t)
+    }
 }
 
 object CompletionUtils {
-  def exceptionally(fn: Throwable => Unit) = new java.util.function.Function[Throwable, Void] {
-    override def apply(e: Throwable): Void = e match {
-      case _: CompletionException => fn(e.getCause).asInstanceOf[Void]
-      case _ => fn(e).asInstanceOf[Void]
+  implicit def exceptionally(fn: Throwable => Unit): JFunction[Throwable, Void] =
+    new JFunction[Throwable, Void] {
+      override def apply(e: Throwable): Void = {
+        // Execute the function.
+        e match {
+          case _: CompletionException => fn(e.getCause)
+          case _                      => fn(e)
+        }
+        // Return null, which is the only value that conforms to Void.
+        // See: https://stackoverflow.com/questions/44171262/convert-scala-unit-to-java-void/44172467#44172467
+        None.orNull
+      }
     }
-  }
 }
