97b66b3c71f4c6f4ba128318ad5962d962f88c31
Luis Miguel Mejía Suárez <luis.miguel.mejia@hotmail.com>
2019-04-21 22:49:38 -0500

    Updating the FS2 implementation of streaming

--- fs2-stream/src/main/scala/neotypes/fs2/implicits.scala
+++ fs2-stream/src/main/scala/neotypes/fs2/implicits.scala
@@ -1,8 +1,10 @@
 package neotypes.fs2
 
 object implicits {
-  implicit def fs2Stream[F[_]](implicit F: cats.effect.Sync[F]): neotypes.Stream[Fs2FStream[F]#T, F] =
-    new neotypes.Stream[Fs2FStream[F]#T, F] {
+  implicit def fs2Stream[_F[_]](implicit F: cats.effect.Sync[_F]): neotypes.Stream.Aux[Fs2FStream[_F]#T, _F] =
+    new neotypes.Stream[Fs2FStream[_F]#T] {
+      override type F[T] = _F[T]
+
       override def init[T](value: () => F[Option[T]]): fs2.Stream[F, T] =
         fs2.Stream.repeatEval(F.suspend(value())).unNoneTerminate
 
