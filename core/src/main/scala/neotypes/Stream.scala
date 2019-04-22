11e6d1811882fc7ff802624b02945eb2a0172cf6
Luis Miguel Mejía Suárez <luis.miguel.mejia@hotmail.com>
2019-04-21 22:45:13 -0500

    Refactor the Stream typeclass to only require one type parameter

--- core/src/main/scala/neotypes/Stream.scala
+++ core/src/main/scala/neotypes/Stream.scala
@@ -1,9 +1,15 @@
 package neotypes
 
-trait Stream[S[_], F[_]] {
+trait Stream[S[_]] {
+  type F[T]
+
   def init[T](value: () => F[Option[T]]): S[T]
 
   def onComplete[T](s: S[T])(f: => F[Unit]): S[T]
 
   def fToS[T](f: F[S[T]]): S[T]
+}
+
+object Stream {
+  type Aux[S[_], _F[_]] = Stream[S] { type F[T] = _F[T] }
 }
