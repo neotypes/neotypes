b9cd6b3835bc6b3e3dcfc308e2cd53a1712bea5e
Luis Miguel Mejía Suárez <luis.miguel.mejia@hotmail.com>
2019-04-22 19:46:10 -0500

    Implementing the Stream typeclass for monix.reactive.Observable

---
+++ monix-stream/src/main/scala/neotypes/monix/stream/implicits.scala
@@ -0,0 +1,23 @@
+package neotypes.monix.stream
+
+import monix.eval.Task
+import monix.reactive.Observable
+
+object implicits {
+  implicit val monixStream: neotypes.Stream.Aux[Observable, Task] =
+    new neotypes.Stream[Observable] {
+      override type F[T] = Task[T]
+
+      override def init[T](value: () => Task[Option[T]]): Observable[T] =
+        Observable
+          .repeatEvalF(Task.suspend(value()))
+          .takeWhile(option => option.isDefined)
+          .collect { case Some(t) => t }
+
+      override def onComplete[T](s: Observable[T])(f: => Task[Unit]): Observable[T] =
+        s.guarantee(f)
+
+      override def fToS[T](f: Task[Observable[T]]): Observable[T] =
+        Observable.fromTask(f).flatten
+    }
+}
