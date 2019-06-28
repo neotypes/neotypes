115d4f5c8750d7808edc5e5380c164daee1b593f
Gabriel Volpe <volpegabriel@gmail.com>
2019-06-28 12:55:39 -0600

    Making test fail by adding a call to flatMap

--- cats-effect/src/test/scala/neotypes/cats/CatsImplicitsSpec.scala
+++ cats-effect/src/test/scala/neotypes/cats/CatsImplicitsSpec.scala
@@ -14,13 +14,14 @@ import org.scalatest.AsyncFlatSpec
 import cats.Monad
 
 class CatsImplicitsSpec extends AsyncFlatSpec with BaseIntegrationSpec {
+
   it should "work with cats implicits and neotypes implicits" in {
     def test1[F[_]: Applicative]: F[Unit] = Applicative[F].unit
 
     def test2[F[_]: Monad]: F[Unit] = ().pure[F]
 
     def makeSession[F[_]: Async]: F[Session[F]] =
-      (test1[F] *> test2[F]).as(driver.session().asScala[F])
+      (test1[F] *> test2[F]).flatMap(_ => driver.session().asScala[F].pure[F])
 
     val s = makeSession[IO].unsafeRunSync()
 
