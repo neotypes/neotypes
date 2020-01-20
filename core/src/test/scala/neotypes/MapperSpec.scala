871ea758ee3c771269ebd63474390031e1f79d71
Luis Miguel Mejía Suárez <luis.miguel.mejia@hotmail.com>
2020-01-20 15:15:43 -0500

    Replacing Seq with List on the ResultMapper

--- core/src/test/scala/neotypes/MapperSpec.scala
+++ core/src/test/scala/neotypes/MapperSpec.scala
@@ -16,60 +16,60 @@ class MapperSpec extends FreeSpec {
         assert(strMapper == StringResultMapper)
       }
       "should create an instance based on a function" in {
-        def myParsingFunction(vals: Seq[(String, Value)], typeHint: Option[TypeHint]) = Right("function works")
+        def myParsingFunction(vals: List[(String, Value)], typeHint: Option[TypeHint]) = Right("function works")
         val myInstanceMapper = ResultMapper.instance(myParsingFunction)
-        val result = myInstanceMapper.to(Seq(("value", new StringValue("function doesn't work"))), None)
+        val result = myInstanceMapper.to(List(("value", new StringValue("function doesn't work"))), None)
         assert(result == Right("function works"))
       }
       "should return a constant result value" in {
         val constMapper = ResultMapper.const[String]("const")
-        val result = constMapper.to(Seq(("value", new StringValue("not const"))), None)
+        val result = constMapper.to(List(("value", new StringValue("not const"))), None)
         assert(result == Right("const"))
       }
       "should return a constant failure" in {
         val myException = new Exception("Example Exception")
         val failureMapper = ResultMapper.failed[String](myException)
-        val result = failureMapper.to(Seq(("value", new StringValue("value"))), None)
+        val result = failureMapper.to(List(("value", new StringValue("value"))), None)
         assert(result == Left(myException))
       }
     }
     "should allow defining a secondary mapper" in {
       val failingMapper = new ResultMapper[String] {
-        override def to(value: Seq[(String, Value)], typeHint: Option[TypeHint]): Either[Throwable, String] =
+        override def to(value: List[(String, Value)], typeHint: Option[TypeHint]): Either[Throwable, String] =
           Left(new Exception)
       }
-      val result = failingMapper.or(StringResultMapper).to(Seq(("value", new StringValue("string"))), None)
+      val result = failingMapper.or(StringResultMapper).to(List(("value", new StringValue("string"))), None)
       assert(result == Right("string"))
     }
 
     "should be mappable" in {
       val caseClassMapper = ResultMapper[String].map(x => MyCaseClass(x + "2"))
-      val result = caseClassMapper.to(Seq(("value", new StringValue("1"))), None)
+      val result = caseClassMapper.to(List(("value", new StringValue("1"))), None)
       assert(result == Right(MyCaseClass("12")))
     }
 
     "should be flatmappable" in {
       val flatMappedMapper = ResultMapper[String].flatMap { str =>
         new ResultMapper[Int] {
-          override def to(value: Seq[(String, Value)], typeHint: Option[TypeHint]): Either[Throwable, Int] =
+          override def to(value: List[(String, Value)], typeHint: Option[TypeHint]): Either[Throwable, Int] =
             Right(str.length)
         }
       }
-      val result = flatMappedMapper.to(Seq(("value", new StringValue("twelve chars"))), None)
+      val result = flatMappedMapper.to(List(("value", new StringValue("twelve chars"))), None)
       assert(result == Right(12))
     }
 
     "should derive a product mapper" in {
       val lengthOfStringMapper = ResultMapper[String].map(_.length)
       val productMapper = StringResultMapper.product(lengthOfStringMapper)
-      val result = productMapper.to(Seq(("value", new StringValue("twelve chars"))), None)
+      val result = productMapper.to(List(("value", new StringValue("twelve chars"))), None)
       assert(result == Right("twelve chars" -> 12))
     }
 
     "should derive an either mapper" in {
       val eitherIntOrStringMapper = ResultMapper[Int].either(StringResultMapper)
-      val resultInt = eitherIntOrStringMapper.to(Seq(("value", new IntegerValue(1))), None)
-      val resultString = eitherIntOrStringMapper.to(Seq(("value", new StringValue("string"))), None)
+      val resultInt = eitherIntOrStringMapper.to(List(("value", new IntegerValue(1))), None)
+      val resultString = eitherIntOrStringMapper.to(List(("value", new StringValue("string"))), None)
       assert(resultInt == Right(Left(1)))
       assert(resultString == Right(Right("string")))
     }
