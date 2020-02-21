e9603309581b715dfadd2837575a6030ef4254f0
Luis Miguel Mejía Suárez <luis.miguel.mejia@hotmail.com>
2020-02-21 18:51:36 -0500

    Making the AsyncGuaranteeSpec generic to F

---
+++ core/src/test/scala/neotypes/AsyncGuaranteeSpec.scala
@@ -0,0 +1,124 @@
+package neotypes
+
+import neotypes.internal.syntax.async._
+
+import org.scalatest.{EitherValues, FlatSpec, Matchers}
+import org.scalatest.compatible.Assertion
+
+/** Base class for testing the Async[F].guarantee method. */
+abstract class AsyncGuaranteeSpec[F[_]] extends FlatSpec with Matchers with EitherValues {
+  def fToEither[T](f: F[T]): Either[Throwable, T]
+  implicit def F: Async[F]
+
+  trait AsyncGuaranteeFixture {
+    private[this] var counter = 0
+
+    final def assertFinalizerWasCalledOnlyOnce: Assertion =
+      withClue("Finalizer was called more than once -") {
+        this.counter shouldBe 1
+      }
+
+    final def assertFinalizerWasNotCalled: Assertion =
+      withClue("Finalizer was called -") {
+        this.counter shouldBe 0
+      }
+
+    final def run[T](result: Either[Throwable, T],
+                     inputEx: Option[Throwable] = None,
+                     finalizerEx: Option[Throwable] = None): Either[Throwable, T] = {
+      def fromOption(opt: Option[Throwable]): F[Unit] =
+        opt.fold(ifEmpty = F.delay(()))(ex => F.failed(ex))
+
+      fToEither(
+        F.guarantee[Unit, T](
+          fa = fromOption(inputEx)
+        )(
+          _ => result.fold(ex => F.failed(ex), t => F.delay(t))
+        ) { (_, _) =>
+          F.delay {
+            counter += 1
+          } flatMap { _ =>
+            fromOption(finalizerEx)
+          }
+        }
+      )
+    }
+  }
+
+  it should "execute finalizer and return the result when nothing fails" in new AsyncGuaranteeFixture {
+    val expectedResult = "result"
+
+    val res = run(result = Right(expectedResult))
+
+    res.right.value shouldBe expectedResult
+    assertFinalizerWasCalledOnlyOnce
+  }
+
+  it should "not execute finalizer and return input exception when input fails" in new AsyncGuaranteeFixture {
+    val inputEx = new Exception("Input failed")
+
+    val ex = run(inputEx = Some(inputEx), result = Right("result"))
+
+    ex.left.value shouldBe inputEx
+    assertFinalizerWasNotCalled
+  }
+
+  it should "execute finalizer and return use exception when use fails" in new AsyncGuaranteeFixture {
+    val useEx = new Exception("Use failed")
+
+    val ex = run(result = Left(useEx))
+
+    ex.left.value shouldBe useEx
+    assertFinalizerWasCalledOnlyOnce
+  }
+
+  it should "not execute finalizer and return input exception when input and use fail" in new AsyncGuaranteeFixture {
+    val inputEx = new Exception("Input failed")
+    val useEx = new Exception("Use failed")
+
+    val ex = run(inputEx = Some(inputEx), result = Left(useEx))
+
+    ex.left.value shouldBe inputEx
+    assertFinalizerWasNotCalled
+  }
+
+  it should "execute finalizer and return finalizer exception when finalizer fails" in new AsyncGuaranteeFixture {
+    val finalizerEx = new Exception("Finalizer failed!")
+
+    val ex = run(result = Right("result"), finalizerEx = Some(finalizerEx))
+
+    ex.left.value shouldBe finalizerEx
+    assertFinalizerWasCalledOnlyOnce
+  }
+
+  it should "not execute finalizer and return input exception when input and finalizer fail" in new AsyncGuaranteeFixture {
+    val inputEx = new Exception("Input failed")
+    val finalizerEx = new Exception("Finalizer failed!")
+
+    val ex = run(inputEx = Some(inputEx), result = Right("result"), finalizerEx = Some(finalizerEx))
+
+    ex.left.value shouldBe inputEx
+    assertFinalizerWasNotCalled
+  }
+
+  it should "execute finalizer and return use exception when use and finalizer fail" in new AsyncGuaranteeFixture {
+    val useEx = new Exception("Input failed")
+    val finalizerEx = new Exception("Finalizer failed!")
+
+    val ex = run(result = Left(useEx), finalizerEx = Some(finalizerEx))
+
+    ex.left.value shouldBe useEx
+    assertFinalizerWasCalledOnlyOnce
+  }
+
+  it should "not execute finalizer and return input exception when all fail" in new AsyncGuaranteeFixture {
+    val inputEx = new Exception("Input failed")
+    val useEx = new Exception("Use failed")
+    val finalizerEx = new Exception("Finalizer failed!")
+
+    val ex = run(inputEx = Some(inputEx), result = Left(useEx), finalizerEx = Some(finalizerEx))
+
+    ex.left.value shouldBe inputEx
+    assertFinalizerWasNotCalled
+  }
+}
--- core/src/test/scala/neotypes/FutureAsyncGuaranteeSpec.scala
+++ core/src/test/scala/neotypes/FutureAsyncGuaranteeSpec.scala
@@ -1,102 +1,15 @@
 package neotypes
 
-import org.scalatest.{FlatSpec, Matchers}
+import java.util.concurrent.Executors
 
-import scala.concurrent.{ExecutionContext, Future}
-import org.scalatest.concurrent.ScalaFutures
-class FutureAsyncGuaranteeSpec extends FlatSpec with Matchers {
-  implicit val ec = ExecutionContext.Implicits.global
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration.Duration
+import scala.util.Try
 
-  trait FutureAsyncFixture {
-    var counter = 0
-    var resourceIsOpen = true
-    val fa = Future.successful(1)
-    val f = (i: Int) => Future.successful(i.toString)
-    val finalizer = (_: Int, _: Option[Throwable]) => Future {
-      counter += 1
-      resourceIsOpen = false
-    }
-  }
+class FutureAsyncGuaranteeSpec extends AsyncGuaranteeSpec[Future] {
+  override def fToEither[T](f: Future[T]): Either[Throwable, T] =
+    Try(Await.result(f, Duration.Inf)).toEither
 
-  it should "execute finalizer" in new FutureAsyncFixture {
-    ScalaFutures.whenReady(Async.futureAsync.guarantee(fa)(f)(finalizer)) {
-      res =>
-        res shouldBe "1"
-        resourceIsOpen shouldBe false // Resource is closed.
-        counter shouldBe 1 // Finalizer was called only once.
-    }
-  }
-
-  it should "execute finalizer when fa fails and return the exception" in new FutureAsyncFixture {
-    val runtimeEx = new Exception("Boom!")
-    override val fa = Future.failed(runtimeEx)
-
-    ScalaFutures.whenReady(Async.futureAsync.guarantee(fa)(f)(finalizer).failed) {
-      ex =>
-        ex shouldBe runtimeEx
-        resourceIsOpen shouldBe false // Resource is closed.
-        counter shouldBe 1 // Finalizer was called only once.
-    }
-  }
-
-  it should "execute finalizer when f fails and return the exception" in new FutureAsyncFixture {
-    val runtimeEx = new Exception("Boom!")
-    override val f = _ => Future.failed(runtimeEx)
-
-    ScalaFutures.whenReady(Async.futureAsync.guarantee(fa)(f)(finalizer).failed) {
-      ex =>
-        ex shouldBe runtimeEx
-        resourceIsOpen shouldBe false // Resource is closed.
-        counter shouldBe 1 // Finalizer was called only once.
-    }
-  }
-
-  it should "return finalizer exception" in new FutureAsyncFixture {
-    val finalizerEx = new Exception("Finalizer Failed!")
-    override val finalizer = (_: Int, _: Option[Throwable]) => {
-      counter += 1
-      Future.failed(finalizerEx)
-    }
-
-    ScalaFutures.whenReady(Async.futureAsync.guarantee(fa)(f)(finalizer).failed){
-      ex =>
-        ex shouldBe finalizerEx
-        resourceIsOpen shouldBe true // Resource was not closed.
-        counter shouldBe 1 // Finalizer was called only once.
-    }
-  }
-
-  it should "execute finalizer and return fa exception when fa and finalizer fail" in new FutureAsyncFixture {
-    val runtimeEx = new Exception("Boom!")
-    val finalizerEx = new Exception("Finalizer Failed!")
-    override val fa = Future.failed(runtimeEx)
-    override val finalizer = (_: Int, _: Option[Throwable]) => {
-      counter += 1
-      Future.failed(finalizerEx)
-    }
-
-    ScalaFutures.whenReady(Async.futureAsync.guarantee(fa)(f)(finalizer).failed) {
-      ex =>
-        ex shouldBe runtimeEx
-        resourceIsOpen shouldBe true // Resource was not closed.
-        counter shouldBe 1 // Finalizer was called only once.
-    }
-  }
-
-  it should "execute finalizer and return f exception when f and finalizer fail" in new FutureAsyncFixture {
-    val runtimeEx = new Exception("Boom!")
-    val finalizerEx = new Exception("Finalizer Failed!")
-    override val f = _ => Future.failed(runtimeEx)
-    override val finalizer = (_: Int, _: Option[Throwable]) => {
-      counter += 1
-      Future.failed(finalizerEx)
-    }
-
-    ScalaFutures.whenReady(Async.futureAsync.guarantee(fa)(f)(finalizer).failed) {
-      ex =>
-        ex shouldBe runtimeEx
-        resourceIsOpen shouldBe true // Resource was not closed.
-        counter shouldBe 1 // Finalizer was called only once.
-    }
-  }
+  override final val F: Async[Future] =
+    Async.futureAsync(ExecutionContext.fromExecutorService(Executors.newSingleThreadExecutor()))
 }
