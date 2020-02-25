5b034d911dc651cd347cda5297dd81748ec91493
Luis Miguel Mejía Suárez <luis.miguel.mejia@hotmail.com>
2020-02-25 12:51:32 -0500

    Migrating the TransactSpec to property based testing

--- monix/src/test/scala/neotypes/monix/MonixTaskTransactSpec.scala
+++ monix/src/test/scala/neotypes/monix/MonixTaskTransactSpec.scala
@@ -1,45 +1,17 @@
-package neotypes.cats.effect
+package neotypes.monix
 
 import monix.eval.Task
 import monix.execution.Scheduler
-import neotypes.TransactIntegrationSpec
-import neotypes.implicits.mappers.all._
-import neotypes.implicits.syntax.string._
-import neotypes.monix.implicits._
-import org.neo4j.driver.v1.exceptions.ClientException
-
-import scala.concurrent.{ExecutionContext, Future}
+import neotypes.{Async, TransactIntegrationSpec}
+import scala.concurrent.Future
 
 class MonixTaskTransactSpec extends TransactIntegrationSpec[Task] { self =>
-  import TransactIntegrationSpec.CustomException
+  implicit val scheduler: Scheduler =
+    Scheduler(self.executionContext)
 
-  implicit val scheduler: Scheduler = Scheduler(self.executionContext)
+  override def fToFuture[T](task: Task[T]): Future[T] =
+    task.runToFuture
 
-  override def fToFuture[T](task: Task[T]): Future[T] = task.runToFuture
-
-  it should "execute & commit multiple queries inside the same transact" in
-    ensureCommitedTransaction(expectedResults = List("Luis", "Dmitry")) { tx =>
-      for {
-        _ <- "CREATE (p: PERSON { name: \"Luis\" })".query[Unit].execute(tx)
-        _ <- "CREATE (p: PERSON { name: \"Dmitry\" })".query[Unit].execute(tx)
-        r <- "MATCH (p: PERSON) RETURN p.name".query[String].list(tx)
-      } yield r
-    }
-
-  it should "automatically rollback if any query fails inside a transact" in
-    ensureRollbackedTransaction[ClientException] { tx =>
-      for {
-        _ <- "CREATE (p: PERSON { name: \"Luis\" })".query[Unit].execute(tx)
-        _ <- "broken cypher query".query[Unit].execute(tx)
-      } yield ()
-    }
-
-  it should "automatically rollback if there is an error inside the transact" in
-    ensureRollbackedTransaction[CustomException] { tx =>
-      for {
-        _ <- "CREATE (p: PERSON { name: \"Luis\" })".query[Unit].execute(tx)
-        _ <- Task.raiseError(CustomException)
-        _ <- "CREATE (p: PERSON { name: \"Dmitry\" })".query[Unit].execute(tx)
-      } yield ()
-    }
+  override final val F: Async[Task] =
+    implicits.monixAsync
 }
