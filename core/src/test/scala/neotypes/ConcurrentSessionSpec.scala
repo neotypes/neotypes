d2b6f7e49909bd5b51e0646c4e5f06a759840bb1
Luis Miguel Mejía Suárez <luis.miguel.mejia@hotmail.com>
2020-04-30 12:13:04 -0500

    Adapting the ConcurrentSessionSpec to the new EffectTestkit

--- core/src/test/scala/neotypes/ConcurrentSessionSpec.scala
+++ core/src/test/scala/neotypes/ConcurrentSessionSpec.scala
@@ -2,25 +2,13 @@ package neotypes
 
 import neotypes.implicits.mappers.all._
 import neotypes.implicits.syntax.cypher._
-import neotypes.internal.syntax.async._
 import org.neo4j.driver.v1.exceptions.ClientException
-import org.scalatest.compatible.Assertion
-import scala.concurrent.Future
-import scala.reflect.ClassTag
 
-/** Base class for testing the concurrent use of a Session[F] */
-abstract class ConcurrentSessionSpec[F[_]] (implicit ct: ClassTag[F[_]]) extends BaseIntegrationSpec[F] {
-  private val effectName: String = ct.runtimeClass.getCanonicalName
+/** Base class for testing the concurrent use of a session. */
+final class ConcurrentSessionSpec[F[_]](testkit: EffectTestkit[F]) extends BaseIntegrationSpec[F](testkit) {
   behavior of s"Concurrent use of Session[${effectName}]"
 
-  def fToFuture[T](f: F[T]): Future[T]
-  def runConcurrently(a: F[Unit], b: F[Unit]): F[Unit]
-  implicit def F: Async[F]
-
-  private final def executeAsFuture[T](work: Session[F] => F[T]): Future[T] =
-    fToFuture(execute(work))
-
-  it should "throw a client exception when session used concurrently" in {
+  it should "throw a client exception when a single session is used concurrently" in {
     recoverToSucceededIf[ClientException] {
       executeAsFuture { s =>
         def query(name: String): DeferredQuery[Unit] =
