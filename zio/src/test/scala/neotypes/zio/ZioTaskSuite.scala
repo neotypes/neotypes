dd146840684ff234e0d27bbc04b2b2adb8555618
geoffjohn11 <geoffjohn11@gmail.com>
2020-05-20 15:43:42 -0400

    fix test to use new default runtime

--- zio/src/test/scala/neotypes/zio/ZioTaskSuite.scala
+++ zio/src/test/scala/neotypes/zio/ZioTaskSuite.scala
@@ -3,16 +3,14 @@ package neotypes.zio
 import neotypes.{Async, EffectSuite, EffectTestkit}
 import neotypes.zio.implicits._
 import scala.concurrent.{ExecutionContext, Future}
-import zio.{DefaultRuntime, Task}
-import zio.internal.PlatformLive
+import zio.{Runtime, Task}
+import zio.internal.Platform
 
 /** Implementation of the Effect Teskit for zio Task. */
 object ZioTaskTestkit extends EffectTestkit[Task] {
   override def createBehaviour(implicit ec: ExecutionContext): Behaviour =
     new Behaviour {
-      val runtime = new DefaultRuntime {
-        override val platform = PlatformLive.fromExecutionContext(ec)
-      }
+      val runtime = Runtime.default.mapPlatform(_ => Platform.fromExecutionContext(ec))
 
       override final def fToFuture[T](task: Task[T]): Future[T] =
         runtime.unsafeRunToFuture(task)
