e27d9b3d0f5281aac01d4454c4e26636c03ee391
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-06-27 19:32:38 +0200

    Update zio, zio-streams to 1.0.0-RC21-1 (#152)
    * Update zio, zio-streams to 1.0.0-RC21-1
    
    * Fixing compilation error
    
    Co-authored-by: Luis Miguel Mejía Suárez <luismiguelmejiasuarez@gmail.com>

--- zio-stream/src/test/scala/neotypes/zio/stream/ZioZStreamsSuite.scala
+++ zio-stream/src/test/scala/neotypes/zio/stream/ZioZStreamsSuite.scala
@@ -11,7 +11,7 @@ object ZioZStreamsTestkit extends StreamTestkit[ZioStream, Task](ZioTaskTestkit)
   override def createBehaviour(implicit ec: ExecutionContext): Behaviour =
     new Behaviour {
       override def streamToFList[T](stream: ZioStream[T]): Task[List[T]] =
-        stream.runCollect
+        stream.runCollect.map(_.toList)
 
       override final val streamInstance: Stream.Aux[ZioStream, Task] =
         implicitly
