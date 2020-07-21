ca22b9f96f922021c9b61c3e89c5bca6dd5da9cd
Luis Miguel Mejía Suárez <luismiguelmejiasuarez@gmail.com>
2020-07-21 10:46:04 -0500

    Making the ConcurrentSessionSpec to check for thread safety

--- core/src/test/scala/neotypes/ConcurrentSessionSpec.scala
+++ core/src/test/scala/neotypes/ConcurrentSessionSpec.scala
@@ -8,17 +8,17 @@ import org.neo4j.driver.exceptions.ClientException
 final class ConcurrentSessionSpec[F[_]](testkit: EffectTestkit[F]) extends BaseIntegrationSpec[F](testkit) {
   behavior of s"Concurrent use of Session[${effectName}]"
 
-  it should "throw a client exception when a single session is used concurrently" in {
-    recoverToSucceededIf[ClientException] {
-      executeAsFuture { s =>
-        def query(name: String): DeferredQuery[Unit] =
-          c"CREATE (p: PERSON { name: ${name} })".query[Unit]
+  it should "work and not throw an exception when a single session is used concurrently" in {
+    executeAsFuture { s =>
+      def query(name: String): DeferredQuery[Unit] =
+        c"CREATE (p: PERSON { name: ${name} })".query[Unit]
 
-        runConcurrently(
-          query(name = "name1").execute(s),
-          query(name = "name2").execute(s)
-        )
-      }
+      runConcurrently(
+        query(name = "name1").execute(s),
+        query(name = "name2").execute(s)
+      )
+    } map { _ =>
+      succeed
     }
   }
 
