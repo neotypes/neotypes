150a1955556d2971be31750623cc481b29fb5845
Luis Miguel Mejía Suárez <luismiguelmejiasuarez@gmail.com>
2020-11-20 17:03:19 -0500

    Releasing the lock in readOnlyTransact

--- core/src/main/scala/neotypes/Session.scala
+++ core/src/main/scala/neotypes/Session.scala
@@ -1,12 +1,12 @@
 package neotypes
 
-import java.util.concurrent.{CompletableFuture, CompletionStage}
-
 import internal.syntax.async._
 import internal.syntax.stage._
 
+import java.util.concurrent.{CompletableFuture, CompletionStage}
+
 import org.neo4j.driver.{TransactionConfig => NeoTransactionConfig}
-import org.neo4j.driver.async.{AsyncTransaction, AsyncTransactionWork, AsyncSession => NeoAsyncSession}
+import org.neo4j.driver.async.{AsyncSession => NeoAsyncSession, AsyncTransaction => NeoAsyncTransaction, AsyncTransactionWork}
 
 sealed trait Session[F[_]] {
   def transaction: F[Transaction[F]]
@@ -53,31 +53,25 @@ object Session {
     override final def readOnlyTransact[T](txf: Transaction[F] => F[T]): F[T] =
       readOnlyTransact(NeoTransactionConfig.empty())(txf)
 
-    override final def readOnlyTransact[T](config: NeoTransactionConfig)(txf: Transaction[F] => F[T]): F[T] = {
-      val fOfT: F[T] = lock.acquire.flatMap { _ =>
-        F.async { cb =>
-          session.readTransactionAsync(new AsyncTransactionWork[CompletionStage[T]] {
-            override def execute(tx: AsyncTransaction) = {
-              val completableF = new CompletableFuture[T]()
-              txf(Transaction(F, tx)(lock)).map(completableF.complete).recover{
-                case th: Throwable => completableF.completeExceptionally(th)
+    override final def readOnlyTransact[T](config: NeoTransactionConfig)(txf: Transaction[F] => F[T]): F[T] =
+      lock.acquire.flatMap { _ =>
+        F.async[F[T]] { cb =>
+          val work: AsyncTransactionWork[CompletionStage[Unit]] = (tx: NeoAsyncTransaction) => {
+            val cf = new CompletableFuture[Unit]()
+
+            cb(Right(
+              F.delay(Transaction(F, tx)(lock)).guarantee(txf) {
+                case (tx, None)    => tx.commit.flatMap(_ => F.delay(internal.utils.void(cf.complete(()))))
+                case (tx, Some(_)) => tx.rollback.flatMap(_ => F.delay(internal.utils.void(cf.complete(()))))
               }
-              completableF
-            }
-          }, config).accept(cb){
-            case t: Throwable => Left(t)
-            case v => Right(v)
+            ))
+
+            cf
           }
-        }
+
+          internal.utils.void(session.readTransactionAsync(work, config).exceptionally(ex => cb(Left(ex))))
+        }.flatMap(identity)
       }
-      //because readTransactionAsync automatically commits the transaction there is no
-      //tx.commit or rollback, so it's necessary to manually release the lock.  Is this the preferred way?
-      fOfT.flatMap{ res =>
-        lock.release.map{_ => res}
-      }.recoverWith{
-        case th: Throwable => lock.release.map(_ => throw th)
-      }
-    }
 
    override final def close: F[Unit] =
       F.async { cb =>
