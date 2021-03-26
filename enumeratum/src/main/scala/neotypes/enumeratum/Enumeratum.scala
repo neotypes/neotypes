0d4e3504cc251075f9910fdf99243323adf88a34
Luis Miguel Mejía Suárez <luismiguelmejiasuarez@gmail.com>
2021-03-26 18:32:11 -0500

    Implementing the Enumeratum module

--- enumeratum/src/main/scala/neotypes/enumeratum/Enumeratum.scala
+++ enumeratum/src/main/scala/neotypes/enumeratum/Enumeratum.scala
@@ -9,71 +9,69 @@ import _root_.enumeratum.values._
 trait NeotypesEnum[EntryType <: EnumEntry] {
   self: Enum[EntryType] =>
 
-  implicit final val parameterMapper: ParameterMapper[EntryType] = ???
-  implicit final val resultMapper: ResultMapper[EntryType] = ???
-  implicit final val valueMapper: ValueMapper[EntryType] = ???
+  implicit final val parameterMapper: ParameterMapper[EntryType] =
+    Impl.parameterMapper[EntryType]
+
+  implicit final val valueMapper: ValueMapper[EntryType] =
+    Impl.valueMapper(self)
+
+  implicit final val resultMapper: ResultMapper[EntryType] =
+    ResultMapper.fromValueMapper[EntryType]
 }
 
 trait NeotypesKeyEnum[EntryType <: EnumEntry] {
   self: Enum[EntryType] =>
 
-  implicit final val keyMapper: KeyMapper[EntryType] = ???
+  implicit final val keyMapper: KeyMapper[EntryType] =
+    Impl.keyMapper(self)
 }
 
 sealed trait NeotypesValueEnum[ValueType, EntryType <: ValueEnumEntry[ValueType]] {
   self: ValueEnum[ValueType, EntryType] =>
 
   implicit def parameterMapper: ParameterMapper[EntryType]
-  implicit def resultMapper: ResultMapper[EntryType]
   implicit def valueMapper: ValueMapper[EntryType]
-}
-
-trait NeotypesByteEnum[EntryType <: ByteEnumEntry] extends NeotypesValueEnum[Byte, EntryType] {
-  self: ValueEnum[Byte, EntryType] =>
-
-  override implicit final val parameterMapper: ParameterMapper[EntryType] = ???
-  override implicit final val resultMapper: ResultMapper[EntryType] = ???
-  override implicit final val valueMapper: ValueMapper[EntryType] = ???
-}
-
-trait NeotypesCharEnum[EntryType <: CharEnumEntry] extends NeotypesValueEnum[Char, EntryType] {
-  self: ValueEnum[Char, EntryType] =>
-
-  override implicit final val parameterMapper: ParameterMapper[EntryType] = ???
-  override implicit final val resultMapper: ResultMapper[EntryType] = ???
-  override implicit final val valueMapper: ValueMapper[EntryType] = ???
+  implicit def resultMapper: ResultMapper[EntryType]
 }
 
 trait NeotypesIntEnum[EntryType <: IntEnumEntry] extends NeotypesValueEnum[Int, EntryType] {
   self: ValueEnum[Int, EntryType] =>
 
-  override implicit final val parameterMapper: ParameterMapper[EntryType] = ???
-  override implicit final val resultMapper: ResultMapper[EntryType] = ???
-  override implicit final val valueMapper: ValueMapper[EntryType] = ???
+  override implicit final val parameterMapper: ParameterMapper[EntryType] =
+    Impl.values.parameterMapper[Int, EntryType]
+
+  override implicit final val valueMapper: ValueMapper[EntryType] =
+    Impl.values.valueMapper(self)
+
+  override implicit final val resultMapper: ResultMapper[EntryType] =
+    ResultMapper.fromValueMapper[EntryType]
 }
 
 trait NeotypesLongEnum[EntryType <: LongEnumEntry] extends NeotypesValueEnum[Long, EntryType] {
   self: ValueEnum[Long, EntryType] =>
 
-  override implicit final val parameterMapper: ParameterMapper[EntryType] = ???
-  override implicit final val resultMapper: ResultMapper[EntryType] = ???
-  override implicit final val valueMapper: ValueMapper[EntryType] = ???
-}
+  override implicit final val parameterMapper: ParameterMapper[EntryType] =
+    Impl.values.parameterMapper[Long, EntryType]
 
-trait NeotypesShortEnum[EntryType <: ShortEnumEntry] extends NeotypesValueEnum[Short, EntryType] {
-  self: ValueEnum[Short, EntryType] =>
+  override implicit final val valueMapper: ValueMapper[EntryType] =
+    Impl.values.valueMapper(self)
 
-  override implicit final val parameterMapper: ParameterMapper[EntryType] = ???
-  override implicit final val resultMapper: ResultMapper[EntryType] = ???
-  override implicit final val valueMapper: ValueMapper[EntryType] = ???
+  override implicit final val resultMapper: ResultMapper[EntryType] =
+    ResultMapper.fromValueMapper[EntryType]
 }
 
 trait NeotypesStringEnum[EntryType <: StringEnumEntry] extends NeotypesValueEnum[String, EntryType] {
   self: ValueEnum[String, EntryType] =>
 
-  override implicit final val parameterMapper: ParameterMapper[EntryType] = ???
-  override implicit final val resultMapper: ResultMapper[EntryType] = ???
-  override implicit final val valueMapper: ValueMapper[EntryType] = ???
+  override implicit final val parameterMapper: ParameterMapper[EntryType] =
+    Impl.values.parameterMapper[String, EntryType]
 
-  implicit final val keyMapper: KeyMapper[EntryType] = ???
+  override implicit final val valueMapper: ValueMapper[EntryType] =
+    Impl.values.valueMapper(self)
+
+  override implicit final val resultMapper: ResultMapper[EntryType] =
+    ResultMapper.fromValueMapper[EntryType]
+
+  implicit final val keyMapper: KeyMapper[EntryType] =
+    Impl.values.keyMapper(self)
 }
