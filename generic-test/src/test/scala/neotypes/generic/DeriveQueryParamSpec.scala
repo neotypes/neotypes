f90fde02749161a44c73bd53b9dd1c886e94498c
Luis Miguel Mejía Suárez <luismiguelmejiasuarez@gmail.com>
2023-04-22 18:29:12 -0500

    Add generic-test: DeriveCaseClassQueryParamSpec

---
+++ generic-test/src/test/scala/neotypes/generic/DeriveQueryParamSpec.scala
@@ -0,0 +1,42 @@
+package neotypes.generic
+
+import neotypes.model.query.QueryParam
+import neotypes.query.{QueryArg, QueryArgMapper}
+import neotypes.generic.implicits.deriveCaseClassQueryParams
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+final class DeriveCaseClassQueryParamSpec extends AnyFlatSpec with Matchers {
+  import DeriveCaseClassQueryParamSpec._
+
+  behavior of "neotypes.implicits.deriveCaseClassQueryParams"
+
+  it should "derive an instance for a case class" in {
+    val mapper = QueryArgMapper[MyCaseClass]
+
+    val input = MyCaseClass("twelve chars", 12)
+    val result = mapper.toArg(input)
+
+    result shouldBe QueryArg.Params(
+      List(
+        "string" -> QueryParam("twelve chars"),
+        "int" -> QueryParam(12)
+      )
+    )
+  }
+
+  it should "not derive an instance for nested classes" in {
+    assertDoesNotCompile("QueryArgMapper[(MyCaseClass, MyCaseClass)]")
+    assertDoesNotCompile("QueryArgMapper[NestedCaseClass]")
+  }
+
+  it should "not derive an instance for an untagged HList" in {
+    assertDoesNotCompile("import shapeless._; QueryArgMapper[Int :: String :: HNil]")
+  }
+}
+
+object DeriveCaseClassQueryParamSpec {
+  final case class MyCaseClass(string: String, int: Int)
+  final case class NestedCaseClass(value: String, nested: MyCaseClass)
+}
